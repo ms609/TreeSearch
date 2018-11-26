bf24c3cca8172c9c0a96941f71220cf62a8f9115
Martin Smith <martins@gmail.com>
2018-11-26 15:16:57 +0000

    Life UniqueSplits and DropSingleSplits from Quartet

--- R/Partitions.R
+++ R/Partitions.R
@@ -47,3 +47,67 @@ Tree2Splits <- function (tr) {
 #' @export
 #' @keywords internal
 Tree2Bipartitions <- Tree2Splits
+
+
+#' Unique Splits
+#' 
+#' Removes equivalent duplicates from a matrix of bipartitions.
+#' 
+#' @template splitsParam
+#' @param preserveParity Logical specifying whether to preserve the `TRUE` and
+#'  `FALSE` status within each split (which takes marginally longer).  If 
+#'  `FALSE`, each split will be defined such that taxa in the same partition
+#'  as the first element are marked `FALSE`, and other taxa marked `TRUE`.
+#'  
+#' @return The splits element, with all duplicate splits removed.
+#' 
+#' @examples 
+#'   set.seed(1)
+#'   splits <- Tree2Splits(ape::rtree(6, br=NULL))
+#'   UniqueSplits(splits, preserveParity=TRUE)
+#' 
+#' @author Martin R. Smith
+#' @export 
+UniqueSplits <- function (splits, preserveParity = FALSE) {
+  originalParity <- splits[1, ]
+  
+  # Set all splits to the same parity
+  splits[, originalParity] <- !splits[, originalParity]
+  
+  # Identify duplicates
+  duplicates <- duplicated(t(splits))
+  
+  # Remove duplicates
+  ret <- splits[, !duplicates, drop=FALSE]
+  
+  # Return: 
+  if (preserveParity) {
+    ret[, originalParity[!duplicates]] <- !ret[, originalParity[!duplicates]]
+  }
+  
+  ret
+}
+
+#' Drop Single Splits
+#' 
+#' Removes splits that pertain only to a single taxon from a splits object.
+#' 
+#' Bipartition splits are divisions, implied by each edge or node of an unrooted
+#' tree topology, that divide the taxa into two groups (one of which is a clade).
+#' 
+#' By default, a list of splits will include those that separate a single taxon
+#' (a leaf) from all others.  Such splits are, by definition, present in all 
+#' trees that contain that taxon; they are not of interest when comparing trees.
+#' This function removes such splits from a list of bipartitions.
+#' 
+#' @param split A matrix in which each column corresponds to a bipartition split
+#' 
+#' @return The input matrix, with any columns that separate only a single pendant
+#'  tip removed.
+#'         
+#' @author Martin R. Smith
+#' 
+#' @export 
+DropSingleSplits <- function (split) {
+  split[, colSums(split) > 1 & colSums(!split) > 1, drop=FALSE]
+}
