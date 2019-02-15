6f25620e3dfa9290a9c97cff008ac4c5f6c902fc
Martin Smith <martins@gmail.com>
2019-02-15 16:35:46 +0000

    Improve test coverage

--- tests/testthat/test-information.R
+++ tests/testthat/test-information.R
@@ -1,5 +1,15 @@
 context("Information.R")
 
+test_that("Information functins work", {
+  expect_equal(NUnrooted(9), TreesMatchingSplit(0, 9))
+  expect_equal(LnUnrooted(9), LogTreesMatchingSplit(0, 9))
+  expect_equal(LnUnrooted(9), LogTreesMatchingSplit(9, 0))
+  expect_equal(log(315/10395)/-log(2), SplitInformation(3, 5))
+  expect_equal(JointInformation(8, 3, 3), SplitInformation(3, 5))
+  expect_equal(-log2(315/10395), JointInformation(8, 3, 0))
+  expect_equal(-log2(135/135135), JointInformation(9, 5, 3))
+})
+
 test_that("UnrootedTreesMatchingSplit works", {
   expect_equal(NRooted(3) * NRooted(5), UnrootedTreesMatchingSplit(c(3, 5)))
   expect_equal(NRooted(30) * NRooted(50), UnrootedTreesMatchingSplit(c(30, 50)))
