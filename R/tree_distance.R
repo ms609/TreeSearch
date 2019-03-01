b1b562256668036a092bdf3d659959905605a61b
Martin Smith <martins@gmail.com>
2019-03-01 15:59:28 +0000

    Variation of Info
    From Melia 2007

--- R/tree_distance.R
+++ R/tree_distance.R
@@ -59,6 +59,35 @@ MutualArborealInfo <- function (tree1, tree2, reportMatching = FALSE) {
 #'
 #' @author Martin R. Smith
 #' @export
+VariationOfInfo <- function (tree1, tree2, reportMatching = FALSE) {
+  if (class(tree1) == 'phylo') {
+    if (class(tree2) == 'phylo') {
+      if (length(setdiff(tree1$tip.label, tree2$tip.label)) > 0) {
+        stop("Tree tips must bear identical labels")
+      }
+      
+      VariationOfSplitInfo(Tree2Splits(tree1), Tree2Splits(tree2), reportMatching)
+    } else {
+      splits1 <- Tree2Splits(tree1)
+      vapply(tree2, 
+             function (tr2) VariationOfSplitInfo(splits1, Tree2Splits(tr2)),
+             double(1))
+    }
+  } else {
+    if (class(tree2) == 'phylo') {
+      splits1 <- Tree2Splits(tree2)
+      vapply(tree1, 
+             function (tr2) VariationOfSplitInfo(splits1, Tree2Splits(tr2)),
+             double(1))
+    } else {
+      splits1 <- lapply(tree1, Tree2Splits)
+      splits2 <- lapply(tree2, Tree2Splits)
+      matrix(mapply(VariationOfSplitInfo, rep(splits1, each=length(splits2)), splits2),
+             length(splits2), length(splits1), dimnames = list(names(tree2), names(tree1)))
+    }
+  }
+}
+
 #' Tree distance based on joint information content of splits
 #' 
 #' #TODO Needs documenting and describing fully.
@@ -177,6 +206,93 @@ MutualArborealInfoSplits <- function (splits1, splits2, reportMatching = FALSE) 
 #' @describeIn VariationOfInfo Takes splits instead of trees
 #' @template splits12params
 #' @export
+VariationOfSplitInfo <- function (splits1, splits2, reportMatching = FALSE) {
+  
+  dimSplits1 <- dim(splits1)
+  dimSplits2 <- dim(splits2)
+  nTerminals <- dimSplits1[1]
+  if (dimSplits2[1] != nTerminals) {
+    stop("Split rows must bear identical labels")
+  }
+  lnUnrootedN <- LnUnrooted.int(nTerminals)
+  
+  if (dimSplits1[2] < dimSplits2[2]) {
+    tmp <- splits1
+    splits1 <- splits2
+    splits2 <- tmp
+    
+    tmp <- dimSplits1
+    dimSplits1 <- dimSplits2
+    dimSplits2 <- tmp
+  }
+  
+  splits2 <- unname(splits2[rownames(splits1), , drop=FALSE])
+  splits1 <- unname(splits1) # split1[split2] faster without names
+  
+  nSplits1 <- dimSplits1[2]
+  nSplits2 <- dimSplits2[2]
+  inSplit1 <- colSums(splits1)
+  inSplit2 <- colSums(splits2)
+  notInSplit1 <- nTerminals - inSplit1
+  notInSplit2 <- nTerminals - inSplit2
+  
+  logTrees1 <- LnRooted(inSplit1) + LnRooted(notInSplit1)
+  logTrees2 <- LnRooted(inSplit2) + LnRooted(notInSplit2)
+  
+  OneOverlap <- function(A1, A2) {
+    if (A1 == A2) {
+      # Return:
+      LnRooted.int(A1) + LnRooted.int(nTerminals - A2)
+    } else {
+      if (A1 < A2) {
+        tmp <- A2
+        A2 <- A1
+        A1 <- tmp
+      }
+      # Return:
+      LnRooted.int(A1) + LnRooted.int(nTerminals - A2) - LnRooted.int(A1 - A2 + 1L) 
+    }
+  }
+  
+  pairScores <- matrix((mapply(function(i, j) {
+    split1 <- splits1[, i]
+    split2 <- splits2[, j]
+    
+    logMutualTrees <- 
+      if (all(oneAndTwo <- split1[split2]) ||
+          all(notOneNotTwo <- !split1[!split2])) {
+        OneOverlap(inSplit1[i], inSplit2[j])
+        
+      } else if (all(notOneAndTwo <- !split1[split2]) ||
+                 all(oneNotTwo <- split1[!split2])) {
+        OneOverlap(inSplit1[i], notInSplit2[j])
+        
+      } else {
+        lnUnrootedN
+      }
+    logTrees1[i] + logTrees2[j] - logMutualTrees - logMutualTrees
+  }, rep(seq_len(nSplits1), each=nSplits2), seq_len(nSplits2)
+  )) / -log(2), nSplits2, nSplits1)
+  
+  
+  
+  if (nSplits2 == 1) {
+    min(pairScores)
+  } else {
+    optimalMatching <- solve_LSAP(pairScores, FALSE)
+    
+    # Return:
+    ret <- sum(pairScores[matrix(c(seq_along(optimalMatching), optimalMatching), ncol=2L)])
+    if (reportMatching) {
+      attr(ret, 'matching') <- optimalMatching
+      attr(ret, 'pairScores') <- pairScores
+      ret
+    } else {
+      ret
+    }
+  }
+}
+
 #' @describeIn MutualArborealInfo Takes splits instead of trees
 #' @inheritParams MutualArborealInfoSplits
 #' @export
