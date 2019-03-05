51642b3c5b68a9275810102c3973390f12f292a0
Martin Smith <martins@gmail.com>
2019-03-05 16:04:29 +0000

    Update test-tree-distance.R

--- tests/testthat/test-tree-distance.R
+++ tests/testthat/test-tree-distance.R
@@ -12,32 +12,43 @@ test_that('Tree differences are correctly calculated', {
   # Labels in different order to confound Tree2Splits
   treeSym8 <- ape::read.tree(text='((e, (f, (g, h))), (((a, b), c), d));')
   treeBal8 <- ape::read.tree(text='(((e, f), (g, h)), ((a, b), (c, d)));')
+  treeOpp8 <- ape::read.tree(text='(((a, f), (c, h)), ((g, b), (e, d)));')
+  
+  treeCat8 <- ape::read.tree(text='((((h, g), f), e), (d, (c, (b, a))));')
+  treeTac8 <- ape::read.tree(text='((((e, c), g), a), (h, (b, (d, f))));')
+  
   treeAb.Cdefgh <- ape::read.tree(text='((a, b), (c, d, e, f, g, h));')
   treeAbc.Defgh <- ape::read.tree(text='((a, b, c), (d, e, f, g, h));')
   treeAbcd.Efgh <- ape::read.tree(text='((a, b, c, d), (e, f, g, h));')
   treeTwoSplits <- ape::read.tree(text="(((a, b), c, d), (e, f, g, h));")
 
   # Labels differ
-  expect_error(InfoTreeDist(treeSym8, ape::read.tree(text='((a, b, c, D), (e, f, g, h));')))
-  expect_equal(22.53747, round(InfoTreeDist(treeSym8, treeSym8), 5))
-  expect_equal(13.75284, round(InfoTreeDist(treeSym8, treeBal8), 5))
-  expect_equal(-log2(945/10395), InfoTreeDist(treeSym8, treeAb.Cdefgh))
-  expect_equal(-log2(315/10395), InfoTreeDist(treeSym8, treeAbc.Defgh))
-  # Test symmetry of small vs large splits
-  expect_equal(InfoTreeDist(treeSym8, treeAbc.Defgh), InfoTreeDist(treeAbc.Defgh, treeSym8))
-  expect_equal(-log2(225/10395), InfoTreeDist(treeSym8, treeAbcd.Efgh))
-  expect_equal(-log2(225/10395) - log2(945/10395),
-               InfoTreeDist(treeSym8, treeTwoSplits))
-  expect_equal(MutualInformation(8, 4, 3),
-               InfoTreeDist(treeTwoSplits, treeAbc.Defgh))
+  expect_error(MutualArborealInfo(treeSym8, ape::read.tree(text='((a, b, c, D), (e, f, g, h));')))
+  expect_equal(22.53747, round(MutualArborealInfo(treeSym8, treeSym8), 5))
+  expect_equal(13.75284, round(MutualArborealInfo(treeSym8, treeBal8), 5))
+  expect_equal(-log2(945/10395), MutualArborealInfo(treeSym8, treeAb.Cdefgh))
+  expect_equal(-log2(315/10395), MutualArborealInfo(treeSym8, treeAbc.Defgh))
   
-  expect_equal(InfoTreeDist(treeSym8, list(treeSym8, treeBal8)), InfoTreeDist(list(treeSym8, treeBal8), treeSym8))
-  expect_equal(matrix(c(InfoTreeDist(treeSym8, treeSym8), InfoTreeDist(treeBal8, treeSym8),
-                      InfoTreeDist(treeSym8, treeAbc.Defgh), InfoTreeDist(treeBal8, treeAbc.Defgh),
-                      InfoTreeDist(treeSym8, treeAbcd.Efgh), InfoTreeDist(treeBal8, treeAbcd.Efgh)),
-                      3L, 2L, byrow=TRUE,
-                      dimnames=list(c('sym', 'abc', 'abcd'), c('sym', 'bal'))), 
-    InfoTreeDist(list(sym=treeSym8, bal=treeBal8), 
+  # Test symmetry of small vs large splits
+  expect_equal(MutualArborealInfo(treeSym8, treeAbc.Defgh),
+               MutualArborealInfo(treeAbc.Defgh, treeSym8))
+  expect_equal(-log2(225/10395), MutualArborealInfo(treeSym8, treeAbcd.Efgh))
+  expect_equal(-log2(225/10395) - log2(945/10395),
+               MutualArborealInfo(treeSym8, treeTwoSplits))
+  expect_equal(SplitMutualInformation(8, 4, 3),
+               MutualArborealInfo(treeTwoSplits, treeAbc.Defgh))
+  
+  expect_equal(MutualArborealInfo(treeSym8, list(treeSym8, treeBal8)), 
+               MutualArborealInfo(list(treeSym8, treeBal8), treeSym8))
+  expect_equal(matrix(c(MutualArborealInfo(treeSym8, treeSym8),
+                        MutualArborealInfo(treeBal8, treeSym8),
+                        MutualArborealInfo(treeSym8, treeAbc.Defgh),
+                        MutualArborealInfo(treeBal8, treeAbc.Defgh),
+                        MutualArborealInfo(treeSym8, treeAbcd.Efgh), 
+                        MutualArborealInfo(treeBal8, treeAbcd.Efgh)),
+                        3L, 2L, byrow=TRUE,
+                        dimnames=list(c('sym', 'abc', 'abcd'), c('sym', 'bal'))), 
+    MutualArborealInfo(list(sym=treeSym8, bal=treeBal8), 
                list(sym=treeSym8, abc=treeAbc.Defgh, abcd=treeAbcd.Efgh)))
   
 })
