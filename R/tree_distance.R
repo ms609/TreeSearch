626d75059fe43d3414e176659a0a97c18c3e861a
Martin Smith <martins@gmail.com>
2019-03-21 09:57:00 +0000

    Use helper function to call tree distance calculators

--- R/tree_distance.R
+++ R/tree_distance.R
@@ -93,63 +93,15 @@
 #' @importFrom clue solve_LSAP
 #' @export
 MutualArborealInfo <- function (tree1, tree2, reportMatching = FALSE) {
-  if (class(tree1) == 'phylo') {
-    if (class(tree2) == 'phylo') {
-      if (length(setdiff(tree1$tip.label, tree2$tip.label)) > 0) {
-        stop("Tree tips must bear identical labels")
-      }
-      
-      MutualArborealInfoSplits(Tree2Splits(tree1), Tree2Splits(tree2), reportMatching)
-    } else {
-      splits1 <- Tree2Splits(tree1)
-      vapply(tree2, 
-             function (tr2) MutualArborealInfoSplits(splits1, Tree2Splits(tr2)),
-             double(1))
-    }
-  } else {
-    if (class(tree2) == 'phylo') {
-      splits1 <- Tree2Splits(tree2)
-      vapply(tree1, 
-             function (tr2) MutualArborealInfoSplits(splits1, Tree2Splits(tr2)),
-             double(1))
-    } else {
-      splits1 <- lapply(tree1, Tree2Splits)
-      splits2 <- lapply(tree2, Tree2Splits)
-      matrix(mapply(MutualArborealInfoSplits, rep(splits1, each=length(splits2)), splits2),
-             length(splits2), length(splits1), dimnames = list(names(tree2), names(tree1)))
-    }
-  }
+  CalculateTreeDistance(MutualArborealInfoSplits, tree1, tree2, 
+                        reportMatching=reportMatching)
 }
 
 #' @describeIn MutualArborealInfo Variation of phylogenetic information between two trees
 #' @export
 VariationOfArborealInfo <- function (tree1, tree2, reportMatching = FALSE) {
-  if (class(tree1) == 'phylo') {
-    if (class(tree2) == 'phylo') {
-      if (length(setdiff(tree1$tip.label, tree2$tip.label)) > 0) {
-        stop("Tree tips must bear identical labels")
-      }
-      
-      VariationOfArborealInfoSplits(Tree2Splits(tree1), Tree2Splits(tree2), reportMatching)
-    } else {
-      splits1 <- Tree2Splits(tree1)
-      vapply(tree2, 
-             function (tr2) VariationOfArborealInfoSplits(splits1, Tree2Splits(tr2)),
-             double(1))
-    }
-  } else {
-    if (class(tree2) == 'phylo') {
-      splits1 <- Tree2Splits(tree2)
-      vapply(tree1, 
-             function (tr2) VariationOfArborealInfoSplits(splits1, Tree2Splits(tr2)),
-             double(1))
-    } else {
-      splits1 <- lapply(tree1, Tree2Splits)
-      splits2 <- lapply(tree2, Tree2Splits)
-      matrix(mapply(VariationOfArborealInfoSplits, rep(splits1, each=length(splits2)), splits2),
-             length(splits2), length(splits1), dimnames = list(names(tree2), names(tree1)))
-    }
-  }
+  CalculateTreeDistance(VariationOfArborealInfoSplits, tree1, tree2, 
+                        reportMatching=reportMatching)
 }
 
 #' @describeIn MutualArborealInfo Mutual clustering information between two trees
@@ -157,41 +109,51 @@ VariationOfArborealInfo <- function (tree1, tree2, reportMatching = FALSE) {
 MutualClusterInfo <- function (tree1, tree2,
                                   reportMatching = FALSE,
                                   bestMatchOnly = TRUE) {
+  CalculateTreeDistance(VariationOfArborealInfoSplits, tree1, tree2, 
+                        reportMatching=reportMatching, bestMatchOnly=bestMatchOnly)
+}
+
+#' Wrapper for tree distance calculations
+#' 
+#' Calls tree distance functions from trees or lists of trees
+#' 
+#' @inheritParams MutualArborealInfo
+#' @param Func Tree distance function.
+#' @param \dots Additional arguments to `Func`.
+#' 
+#' @author Martin R. Smith
+#' @keywords internal
+#' @export
+CalculateTreeDistance <- function (Func, tree1, tree2, reportMatching, ...) {
   if (class(tree1) == 'phylo') {
     if (class(tree2) == 'phylo') {
       if (length(setdiff(tree1$tip.label, tree2$tip.label)) > 0) {
         stop("Tree tips must bear identical labels")
       }
       
-      MutualClusterInfoSplits(Tree2Splits(tree1), Tree2Splits(tree2),
-                                    reportMatching, bestMatchOnly)
+      Func(Tree2Splits(tree1), Tree2Splits(tree2), reportMatching, ...)
     } else {
       splits1 <- Tree2Splits(tree1)
       vapply(tree2, 
-             function (tr2) MutualClusterInfoSplits(splits1, Tree2Splits(tr2),
-                                                      reportMatching,
-                                                      bestMatchOnly),
+             function (tr2) Func(splits1, Tree2Splits(tr2), ...),
              double(1))
     }
   } else {
     if (class(tree2) == 'phylo') {
       splits1 <- Tree2Splits(tree2)
       vapply(tree1, 
-             function (tr2) MutualClusterInfoSplits(splits1, Tree2Splits(tr2),
-                                                          reportMatching, bestMatchOnly),
+             function (tr2) Func(splits1, Tree2Splits(tr2), ...),
              double(1))
     } else {
       splits1 <- lapply(tree1, Tree2Splits)
       splits2 <- lapply(tree2, Tree2Splits)
-      matrix(mapply(MutualClusterInfoSplits,
-                    rep(splits1, each=length(splits2)),
-                    splits2,
-                    reportMatching=reportMatching,
-                    bestMatchOnly= bestMatchOnly),
-             length(splits2), length(splits1), dimnames = list(names(tree2), names(tree1)))
+      matrix(mapply(Func, rep(splits1, each=length(splits2)), splits2),
+             length(splits2), length(splits1),
+             dimnames = list(names(tree2), names(tree1)), ...)
     }
   }
 }
+
 
 #' @describeIn MutualArborealInfo Takes splits instead of trees
 #' @export
