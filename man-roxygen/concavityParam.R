dd26f928c0cd668e2c7129ab07187743e47f0f9f
Martin Smith <martins@gmail.com>
2019-09-09 13:52:27 +0100

    Blank branch for gh-pages

--- man-roxygen/concavityParam.R
+++
@@ -1,3 +0,0 @@
-#' @param concavity A numeric value to use as the concavity constant (`k`)
-#'  in implied weighting. Defaults to 10; TNT sets a default of 3, but this 
-#'  is too low in some circumstances (Smith, 2019).
