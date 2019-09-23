63eada2371a6074fdad48dcd9a42f244562c74dc
Martin Smith <martins@gmail.com>
2019-09-23 11:34:34 +0100

    MinimumLength

--- tests/testthat/test-data-manipulation.R
+++ tests/testthat/test-data-manipulation.R
@@ -1,16 +1,22 @@
 context("data_manipulation.R")
 test_that("minimum step counts are correctly calculated", {
-  expect_equal(1, MinimumSteps(1:3))
-  expect_equal(1, MinimumSteps(c(1:3, 5)))
-  expect_equal(0, MinimumSteps(c(6, 7, 14)))
-  expect_equal(1, MinimumSteps(0:3)) # 0 representing the inapplicable token
+  expect_equal(1, MinimumLength(1:3))
+  expect_equal(1, MinimumLength(c(1:3, 5)))
+  expect_equal(0, MinimumLength(c(6, 7, 14)))
+  expect_equal(1, MinimumLength(0:3)) # 0 representing the inapplicable token
+  
+  tr <- ape::read.tree(text='(((a, b), c), (d, (e, ((f, g), (h, (i, (j, k)))))));')
+  expect_equal(CharacterLength(tr, StringToPhyDat('11---22--33', letters[1:11])),
+               MinimumLength(c(0, 0, 0, 0, 0, 0, 2, 2, 4, 4, 8, 8)))
 
+  plot()
+  
   
   # 04, 14, 24, 34, 05, 16, 27, 38, 9A
   # In this case, chosing the most common state (4) means that we have to choose 567&8 too
   # 012&3 is a better solution
   # We also have to choose one of 9 or A, but it doesn't matter which.
-  expect_equal(4, MinimumSteps(c(
+  expect_equal(4, MinimumLength(c(
     2^0 + 2^4,
     2^1 + 2^4,
     2^2 + 2^4,
@@ -21,6 +27,5 @@ test_that("minimum step counts are correctly calculated", {
     2^3 + 2^8,
     2^9 + 2^10
   )))
-  
   
 })
