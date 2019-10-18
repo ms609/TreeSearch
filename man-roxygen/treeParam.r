027664be2da021ee52998f80af9d2bf3591fb302
ms609 <martins@gmail.com>
2019-10-18 11:00:14 +0100

    Move functions to `TreeTrunk`

--- man-roxygen/treeParam.r
+++ man-roxygen/treeParam.r
@@ -1,1 +1,2 @@
 #' @param tree A tree of class \code{\link{phylo}}.
+# Defined in TreeTrunk.  Please propagate any changes there.
--- man-roxygen/treeParent.r
+++ man-roxygen/treeParent.r
@@ -1,2 +1,3 @@
 #' @param parent the first column of the edge matrix of a tree of class
 #'  \code{\link{phylo}}, i.e. `tree$edge[, 1]`
+# Defined in TreeTrunk.  Please propagate any changes there.
