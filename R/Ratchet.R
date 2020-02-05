ce815aa2d032199a173d98be7f6c22c1f0712d7d
Martin Smith <martins@gmail.com>
2020-02-05 13:54:02 +0000

    Link TreeTools

--- R/Ratchet.R
+++ R/Ratchet.R
@@ -39,14 +39,14 @@
 #'
 #' @examples
 #' data('Lobo', package='TreeTools')
-#' njtree <- NJTree(Lobo.phy)
+#' njtree <- TreeTools::NJTree(Lobo.phy)
 #' # Increase value of ratchIter and searchHits to do a proper search
-#' quickResult <- Ratchet(njtree, Lobo.phy, ratchIter=2, searchHits=3)
+#' quickResult <- Ratchet(njtree, Lobo.phy, ratchIter = 2, searchHits = 3)
 #' plot(quickResult)
 #' # IW search is currently much slower:
-#' quickIWResult <- IWRatchet(quickResult, Lobo.phy, concavity=2.5,
-#'                            ratchIter=1, searchIter = 25, searchHits=2,
-#'                            swappers=RootedTBRSwap, verbosity=5)
+#' quickIWResult <- IWRatchet(quickResult, Lobo.phy, concavity = 2.5,
+#'                            ratchIter = 1, searchIter = 25, searchHits = 2,
+#'                            swappers = RootedTBRSwap, verbosity = 5)
 #'  
 #' @author Martin R. Smith
 #' 
