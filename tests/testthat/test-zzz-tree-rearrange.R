981e6daa1386fbbd0a58adbfc599934671568f3b
ms609 <martins@gmail.com>
2020-02-05 12:38:51 +0000

    Move Preorder tests to TreeTools

--- tests/testthat/test-zzz-tree-rearrange.R
+++ tests/testthat/test-zzz-tree-rearrange.R
@@ -14,28 +14,15 @@ test_that("Malformed trees don't crash anything", {
   treePolytomy   <- read.tree(text = "((((1,2,3),4),5),6);")
   treeDoublyPoly <- read.tree(text = "(((((1,2,3)),4),5),6);")
 
-  reordered <- Preorder(treeDoubleNode)$edge
-  expect_equal(10L, dim(reordered)[1])
-  expect_true(all(table(reordered[, 1]) == 2L))
-  
-  reordered <- Preorder(treePolytomy)$edge
-  expect_equal(9L, dim(reordered)[1])
-  expect_equal(c(2L, 2L, 2L, 3L), as.integer(table(reordered[, 1])))
-  
-  reordered <- Preorder(treeDoublyPoly)$edge
-  expect_equal(9L, dim(reordered)[1])
-  expect_equal(c(2L, 2L, 2L, 3L), as.integer(table(reordered[, 1])))
-  
-  
   expect_error(NNI(treeDoubleNode))
   expect_error(NNI(treePolytomy))
   expect_error(NNI(treeDoublyPoly))
   
-  expect_equal('phylo', class(SPR(treeDoubleNode))) # collapse doubles in Preorder
+  expect_error(SPR(treeDoubleNode))
   expect_error(SPR(treePolytomy))
   expect_error(SPR(treeDoublyPoly))
   
-  expect_equal('phylo', class(TBR(treeDoubleNode))) # collapse doubles in Preorder
+  expect_error(TBR(treeDoubleNode))
   expect_error(TBR(treePolytomy))
   expect_error(TBR(treeDoublyPoly))
   
