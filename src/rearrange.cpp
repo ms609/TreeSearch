6c01290674cd2d72ea87f56d0d35baac6c187ef8
Martin Smith <martins@gmail.com>
2020-10-12 11:06:36 +0100

    all_tbr Seems to work(!)

--- src/rearrange.cpp
+++ src/rearrange.cpp
@@ -325,8 +325,7 @@ List all_tbr (const IntegerMatrix edge,
     n_internal = n_edge / 2,
     n_tip = n_internal + 1,
     n_vert = n_internal + n_tip,
-    root_node = n_tip + 1,
-    second_root_child = root_node + 1
+    root_node = n_tip + 1
   ;
   if (n_edge < 5) throw std::invalid_argument("No TBR rearrangements possible on a tree with < 5 edges");
   if (edge(0, 0) != root_node) throw std::invalid_argument("edge[1,] must connect root to leaf. Try Preorder(root(tree)).");
@@ -379,12 +378,11 @@ List all_tbr (const IntegerMatrix edge,
       break_child = edge(break_edge, 1),
       spare_node = break_parent,
       
+      fragment_root = break_child,
       fragment_leaves = count_children(n_children, break_child),
       fragment_edges = fragment_leaves + fragment_leaves - 1,
       fragment_min_edge = break_edge,
-      fragment_max_edge = break_edge + fragment_edges - 1,
-      base_leaves = n_tip - fragment_leaves,
-      base_edges = n_edge - fragment_edges - 1 // -1 for the broken edge
+      fragment_max_edge = break_edge + fragment_edges - 1
     ;
     const bool broken_on_left = get_child(left_edge, break_parent, n_tip) == break_edge;
     const int16
@@ -420,9 +418,61 @@ List all_tbr (const IntegerMatrix edge,
         ret.push_back(new_tree);
       }
     } else {
-      for (int16 loose_root = fragment_edges; loose_root--; ) {
-        for (int16 graft_location = base_edges; graft_location--; ) {
+      List rooty_bits = List::create();
+      const int16 
+        fragment_base_right = break_edge + 1,
+        fragment_base_left = get_child(left_edge, fragment_root, n_tip);
+      ; 
+      
+      for (int16 new_fragment_root = fragment_min_edge + 2;
+           new_fragment_root != fragment_max_edge + 1; new_fragment_root++) {
+        if (new_fragment_root == fragment_base_left) {
+          rooty_bits.push_back(two_bits);
+          continue;
+        }
+        
+        const int16 new_root_parent = two_bits(new_fragment_root, 0);
+        int16 invert_next = new_fragment_root;
+        IntegerMatrix rerooted = clone(two_bits);
+        
+        rerooted(invert_next, 0) = break_child; // Borrow fragment-root node id
+        rerooted(invert_next, 1) = two_bits(invert_next, 0);
+        
+        do {
+          invert_next = edge_above(two_bits(invert_next, 0), parent_edge);
+          rerooted(invert_next, 0) = two_bits(invert_next, 1);
+          rerooted(invert_next, 1) = two_bits(invert_next, 0);
+        } while (two_bits(invert_next, 0) != fragment_root);
+        
+        const bool new_root_on_right = invert_next == fragment_base_right;
+        const int16 repurposed_edge = new_root_on_right ?
+          fragment_base_left : fragment_base_right;
+        rerooted(invert_next, 1) = two_bits(repurposed_edge, 1);
+        rerooted(repurposed_edge, 1) = two_bits(new_fragment_root, 1);
+        rooty_bits.push_back(rerooted);
+      }
+      for (int16 graft_edge = n_edge - 1; graft_edge; graft_edge--) {
+        if (graft_edge == fragment_max_edge) {
+          // Remember: graft_location will be decremented after continue
+          graft_edge = fragment_min_edge;
+          continue; 
+        } else if (broken_on_left && graft_edge == get_child(right_edge, break_parent, n_tip)) {
+          // Remember: graft_location will be incremented after continue
+          graft_edge = edge_above(break_parent, parent_edge);
+          continue;
+        } else if (graft_edge == spare_edge) {
+          continue;
+        } else if (graft_edge == edge_above(break_parent, parent_edge)) {
+          continue;
+        }
+        for (List::iterator j = rooty_bits.begin(); j != rooty_bits.end(); j++) {
+          IntegerMatrix rooty_bit = *j;
+          IntegerMatrix new_tree = clone(rooty_bit);
           
+          new_tree(spare_edge, 1) = two_bits(graft_edge, 1);
+          new_tree(graft_edge, 1) = spare_node;
+          // Rcout << "Leading to :\n";
+          // print_edges(new_tree);
         }
       }
     }
