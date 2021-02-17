06bdafbda046028943fd20020c84f879acc95e80
Martin Smith <martins@gmail.com>
2021-02-17 16:53:27 +0000

    Update with Carter exact calculation

--- R/pp_info_extra_step.r
+++ R/pp_info_extra_step.r
@@ -1,46 +1,29 @@
-#' Information Content Steps
+#' Information Content of a character known to contain _e_ steps
 #'
-#' `ICSteps()` estimates the phylogenetic information content of a character 
-#' `char` when _e_ extra steps are present, for all possible values of _e_.
+#' `StepInformation()` calculates the phylogenetic information content of a
+#' character `char` when _e_ extra steps are present, for all possible 
+#' values of _e_.
 #' 
 #' Calculates the number of trees consistent with the character having 
 #' _e_ extra steps, where _e_ ranges from its minimum possible value
 #' (i.e. number of different tokens minus one) to its maximum.
-#' The number of trees with no extra steps can be calculated exactly; the 
-#' number of trees with more additional steps must be approximated.
-#' The function samples `maxIter` trees, or enough trees that the trees with 
-#' the minimum number of steps will be recovered at least `expectedMinima`
-#' times, in order to obtain precise results.
 #'
 #' @param char Vector of tokens listing states for the character in question.
-#' @param ambiguousToken Which token, if any, corresponds to the ambiguous token
-#' (?) (not yet fully implemented).
-#' @param tolerance Numeric specifying an anticipated upper bound on the
-#' standard error of approximate profile scores.
-#' @param maxIter Integer specifying maximum number of trees to score when
-#' approximating profile scores.  Small values may result in errors
-#' exceeding `tolerance`.
-#' @template warnParam
+#' @param ambiguousToken Which tokens, if any, correspond to the ambiguous token
+#' (?).
 #' 
-#' @template MRS
 #' @references
 #'  \insertRef{Faith2001}{TreeSearch}
 #' 
-#' @keywords tree
-#' 
 #' @examples
-#' character <- rep(c(0:3, '?'), c(8, 5, 1, 1, 2))
-#' ICSteps(character, warn = FALSE)
-#' 
-#' @importFrom TreeTools NUnrooted Log2Unrooted Log2UnrootedMult NUnrootedMult
+#' character <- rep(c(0:3, '?', '-'), c(8, 5, 1, 1, 2, 2))
+#' StepInformation(character)
+#' @template MRS
+#' @importFrom TreeTools Log2Unrooted
 #' @export
-ICSteps <- function (char, ambiguousToken = '?', tolerance = 0.05,
-                     maxIter = 40000L, warn = TRUE) {
-  #char <- matrix(2L ^ char[char != ambiguousToken], ncol = 1L)
-  char <- char[char != ambiguousToken]
-  names(char) <- paste0('t', seq_along(char))
-  
-  split <- sort(as.integer(table(char)))
+StepInformation <- function (char, ambiguousTokens = c('-', '?')) {
+  char <- char[!char %in% ambiguousTokens]
+  split <- sort(as.integer(table(char)), decreasing = TRUE)
   singletons <- split == 1L
   nSingletons <- sum(singletons)
   nInformative <- sum(split) - nSingletons
@@ -50,81 +33,13 @@ ICSteps <- function (char, ambiguousToken = '?', tolerance = 0.05,
   split <- split[!singletons]
   if (length(split) < 2L) return (1L)
   
-  
-  nNoExtraSteps <- NUnrootedMult(split)
-  log2ExtraSteps <- LnUnrootedMult(split)
-  nOneExtraStep <- WithOneExtraStep(split)
-  
-  p1 <- nOneExtraStep / NUnrooted(sum(split))
-  ic1 <- -log2(nOneExtraStep / NUnrooted(sum(split)))
-  icTol <- tolerance - ic1
-  pTol <- (2 ^ icTol) - p1
-  iter <- p1 * (1 - p1) / (pTol ^ 2)
-  
-  nIter <- min(maxIter, round(iter))
-  if (nIter == maxIter && warn) {
-    warning ("Will truncate number of iterations at maxIter = ", maxIter)
-  }
-  n01ExtraSteps <- nOneExtraStep + nNoExtraSteps
-  analyticIC <- Log2Unrooted(sum(split)) -  setNames(c(
-    Log2UnrootedMult(split), log2(n01ExtraSteps)),
-    minSteps + 0:1)
-  analyticP <- 2 ^ -analyticIC[2]
-  
-  if (warn) {
-    message('  Token count ', split, " = ",
-            signif(analyticIc0, ceiling(log10(maxIter))),
-            ' bits @ 0 extra steps. \n  Simulating ', nIter, 
-            ' trees to estimate cost of further steps.')
-    # message(c(round(analyticIc0, 3), 'bits @ 0 extra steps;', round(analyticIc1, 3),
-    #    '@ 1; attempting', nIter, 'iterations.\n'))
-  }
-  
-  morphyObj <- SingleCharMorphy(rep(seq_along(split) - 1L, split))
-  on.exit(morphyObj <- UnloadMorphy(morphyObj))
-  steps <- vapply(rep(nInformative, maxIter), RandomTreeScore,
-                  integer(1), morphyObj)
-  
-  tabSteps <- table(steps[steps > (minSteps - nSingletons + 1)]) # Quicker than table(steps)[-1]
-  
-  approxP <- tabSteps / sum(tabSteps) * (1 - analyticP)
-  approxSE <- sqrt(approxP * (1 - approxP) / nIter)
-  cumP <- cumsum(c(analyticP, approxP))[-1]
-                 
-  approxIC <- -log2(cumP)
-  icLB <- -log2(cumP - approxSE)
-  icError <- icLB - approxIC
-  if (warn || max(icError) > tolerance) {
-    message("  Approx. std. error < ", signif(max(icError) * 1.01, 2))
-  }
-  ret <- c(analyticIC, approxIC)
+  profile <- vapply(seq_len(split[2]), Carter1, double(1), split[1], split[2])
+  ret <- setNames(Log2Unrooted(sum(split[1:2])) - log2(cumsum(profile)),
+                  seq_len(split[2]) + sum(singletons))
   ret[length(ret)] <- 0 # Floating point error inevitable
   
   # Return:
   ret
-}
-
-#' @describeIn ICPerStep Memoized calculating function
-#'
-#' @param a \code{min(splits)}.
-#' @param b \code{max(splits)}.
-#' @param m \code{maxIter}.
-#' @template warnParam
-#'
-#' @importFrom R.cache addMemoization
-#' @keywords internal
-#' @export
-ICS <- addMemoization(function(a, b, m, warn = TRUE) {
-  ICSteps(c(rep(1, a), rep(2, b)), maxIter = m, warn = warn)
-})
-
-#' Information content per step
-#' @template splitsParam
-#' @param maxIter number of iterations to use when estimating concavity constant
-#' @template warnParam
-#' @export
-ICPerStep <- function(splits, maxIter, warn = TRUE) {
-  ICS(min(splits), max(splits), maxIter, warn)
 }
 
 #' Number of trees with _m_ additional steps
@@ -133,7 +48,7 @@ ICPerStep <- function(splits, maxIter, warn = TRUE) {
 #' where _a_ leaves are labelled with one state, and _b_ leaves labelled with
 #' a second state.
 #' 
-#' Implementation of theorem 1 from Carter _et al._ (1990)
+#' Implementation of theorem 1 from Carter _et al._ (1990).
 #' 
 #' @param m Number of steps
 #' @param a,b Number of leaves labelled `0` and `1`.
@@ -192,7 +107,49 @@ Log2Carter1 <- function (m, a, b) {
   ) - Log2DoubleFactorial(twoN - twoM - 1L)
 }
 
-# TODO: Remove the below, or if necessary replace with results of Carter et al. 1990; Steel 1993
+# TODO: Replace the below with an advanced version of Maddison & Slakey 1991, 
+# or use the results of Carter et al. 1990; Steel 1993 to estimate +0 & +1 steps,
+# and approximate the rest.
+
+Old_IC_Approx <- function() {
+  
+  nIter <- min(maxIter, round(iter))
+  if (nIter == maxIter && warn) {
+    warning ("Will truncate number of iterations at maxIter = ", maxIter)
+  }
+  n01ExtraSteps <- nOneExtraStep + nNoExtraSteps
+  analyticIC <- Log2Unrooted(sum(split)) -  setNames(c(
+    Log2UnrootedMult(split), log2(n01ExtraSteps)),
+    minSteps + 0:1)
+  analyticP <- 2 ^ -analyticIC[2]
+  
+  if (warn) {
+    message('  Token count ', split, " = ",
+            signif(analyticIc0, ceiling(log10(maxIter))),
+            ' bits @ 0 extra steps. \n  Simulating ', nIter, 
+            ' trees to estimate cost of further steps.')
+    # message(c(round(analyticIc0, 3), 'bits @ 0 extra steps;', round(analyticIc1, 3),
+    #    '@ 1; attempting', nIter, 'iterations.\n'))
+  }
+  
+  morphyObj <- SingleCharMorphy(rep(seq_along(split) - 1L, split))
+  on.exit(morphyObj <- UnloadMorphy(morphyObj))
+  steps <- vapply(rep(nInformative, maxIter), RandomTreeScore,
+                  integer(1), morphyObj)
+  
+  tabSteps <- table(steps[steps > (minSteps - nSingletons + 1)]) # Quicker than table(steps)[-1]
+  
+  approxP <- tabSteps / sum(tabSteps) * (1 - analyticP)
+  approxSE <- sqrt(approxP * (1 - approxP) / nIter)
+  cumP <- cumsum(c(analyticP, approxP))[-1]
+  
+  approxIC <- -log2(cumP)
+  icLB <- -log2(cumP - approxSE)
+  icError <- icLB - approxIC
+  if (warn || max(icError) > tolerance) {
+    message("  Approx. std. error < ", signif(max(icError) * 1.01, 2))
+  }
+}
 
 #' Number of trees with one extra step
 #' @param \dots Vector or series of integers specifying the number of leaves
@@ -382,55 +339,4 @@ Evaluate <- function (tree, dataset, warn=TRUE) {
           round(info.needed, 2), ' needed.  SNR = ', signal.noise, "\n")
   # Return:
   c(signal.noise, info.retained/info.needed)
-}
-
-#' Amount of information in each character
-#'
-#' As presently implemented, this function requires that there be no ambiguous
-#' tokens and two applicable tokens, '1' and '2'.
-#'
-#' @param tokenTable A matrix, where each row corresponds to a character, each
-#' column to a leaf, and each entry to the value (1 or 2) of the character at 
-#' that leaf.
-#' @param precision Integer specifyign how many random trees to generate when 
-#' calculating profile curves.
-#' @template warnParam
-#'
-#' @return information content of each extra step, in bits
-#' 
-#' @author Martin R. Smith
-#'
-#' @export
-InfoAmounts <- function (tokenTable, precision = 1e+06, warn = TRUE) {
-  # The below is simplified from info_extra_step.r::evaluate
-  if (length(unique(as.integer(tokenTable))) > 2L) {
-    stop ("Cannot calculate information amouts for",
-          "characters unless only tokens are 1 and 2. See ?InfoAmounts().")
-  }
-  splits <- apply(tokenTable, 1, table)
-  infoLosses <- apply(splits, 2, ICPerStep, maxIter = precision, warn = warn)
-  
-  blankReturn <- double(max(colSums(splits)))
-  ret <- vapply(infoLosses, function(p) {
-    calcIC <- blankReturn
-    cumP <- cumsum(p)
-    nSteps <- as.integer(names(p))
-    infer <- min(nSteps):max(nSteps)
-    infer <- infer[!(infer %in% nSteps)]
-    calculatedP <- double(max(nSteps))
-    calculatedP[nSteps] <- cumP
-    if (length(infer)) {
-      if (cumP[infer[1] - 1L] > 0.999998) {
-        # We can cope with rounding at the sixth decimal place, I think
-        calculatedP[infer] <- 1
-      } else {
-        fitL <- nls(cumP ~ SSlogis(nSteps, Asym, xmid, scal))
-        calculatedP[infer] <- LogisticPoints(infer, fitL)
-        ##if (warn) warning('Concavity function generated by approximation')
-      }
-    }
-    calcIC[seq_along(calculatedP)] <- -log(calculatedP) / log(2)
-    calcIC
-  }, blankReturn)
-  ret[rowSums(ret) > 0, ]
 }
