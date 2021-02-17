6c312f2deef1ac9b66d24e300a9d739eaed005be
Martin Smith <martins@gmail.com>
2021-02-17 12:31:16 +0000

    Maddison & Slatkin 1991 approach

--- R/pp_info_extra_step.r
+++ R/pp_info_extra_step.r
@@ -193,6 +193,175 @@ Log2Carter1 <- function (m, a, b) {
 }
 
 
+#' @param n Number of leaves in tree
+#' @param x Vector specifying number of leaves with state 1, 2, ...
+#' @param b State reconstructed at base of tree
+#' States are converted to binary, so 
+#' `1` denotes 'state 1',
+#' `2` denotes 'state 2', and
+#' `3` denotes 'state 1 OR state 2'.
+#' 
+#' @references
+#' \insertRef{Maddison1991}{TreeSearch}
+#' @examples
+#' @template MRS
+#' @importFrom TreeTools NRooted
+#' @export
+MaddisonSlatkin <- function (a, b) {
+  n <- sum(a, b)
+  i <- min(a, b)
+  
+  D <- function (j, i, m, n) {
+    choose(i, j) * choose(n - i, m - j) / choose(n, m)
+  }
+  R <- function (m, n) {
+    prod(ifelse(n == m + m, 1L, 2L),
+         choose(n, m),
+         NRooted(m),
+         NRooted(n - m)
+    ) / NRooted(n)
+  }
+  .B0 <- function (n, i) {
+    if (n == 2L && i == 1L) {
+      0
+    } else if (n == i) {
+      0
+    } else if (i == 0L) {
+      1
+    } else {
+      sum(vapply(seq_len(n / 2L), function (m) {
+        R(m, n) * sum(vapply(seq_len(min(i, m)), function (j) {
+          D(j, i, m, n) * sum(
+            .B0(m, j) * .B0(n - m, i - j),
+            .B01(m, j) * .B0(n - m, i - j),
+            .B0(m, j) * .B01(n - m, i - j)
+          )
+        }, double(1)))
+      }, double(1)))
+    }
+  }
+  .B1 <- function (n, i) {
+    if (n == 2L && i == 1L) {
+      0
+    } else if (n == i) {
+      1
+    } else if (i == 0L) {
+      0
+    } else {
+      sum(vapply(seq_len(n / 2L), function (m) {
+        R(m, n) * sum(vapply(seq_len(min(i, m)), function (j) {
+          D(j, i, m, n) * sum(
+            .B1(m, j) * .B1(n - m, i - j),
+            .B01(m, j) * .B1(n - m, i - j),
+            .B1(m, j) * .B01(n - m, i - j)
+          )
+        }, double(1)))
+      }, double(1)))
+    }
+  }
+  .B01 <- function (n, i) {
+    if (n == 2L && i == 1L) {
+      1
+    } else if (n == i) {
+      0
+    } else if (i == 0L) {
+      0
+    } else {
+      sum(vapply(seq_len(n / 2L), function (m) {
+        R(m, n) * sum(vapply(seq_len(min(i, m)), function (j) {
+          D(j, i, m, n) * sum(
+            .B01(m, j) * .B01(n - m, i - j),
+            .B0(m, j) * .B1(n - m, i - j),
+            .B1(m, j) * .B0(n - m, i - j)
+          )
+        }, double(1)))
+      }, double(1)))
+    }
+  }
+  .PRow <- function (P1, B1, P2, B2, i, j, m, n, s, rMax = s) {
+    sum(vapply(0:rMax, function (r) prod(
+      P1(r, m, j),
+      B1(m, j),
+      P2(s - r, n - m, i - j),
+      B2(n - m, i - j)), double(1)))
+  }
+  .P0 <- function (s, n, i) {
+    if (n == 2L && i == 1L) {
+      if (s == 1L) 1 else 0
+    } else if (n == i) {
+      0 # Undefined?
+    } else if (i == 0L) {
+      if (s == 0L) 1 else 0
+    } else {
+      b0 <- .B0(n, i)
+      if (b0 == 0) 0 else {
+        vapply(seq_len(n / 2), function (m) {
+          R(m, n) / b0 * sum(vapply(seq_len(min(i, m)), function (j) {
+            D(j, i, m, n) * sum(
+              .PRow(.P0, .B0, .P0, .B0, i, j, m, n, s),
+              .PRow(.P0, .B0, .P01, .B01, i, j, m, n, s),
+              .PRow(.P01, .B01, .P0, .B0, i, j, m, n, s)
+              )
+          }, double(1)))
+        }, double(1))
+      }
+    }
+  }
+  .P1 <- function (s, n, i) {
+    if (n == 2L && i == 1L) {
+      0
+    } else if (n == i) {
+      1
+    } else if (i == 0L) {
+      0
+    } else {
+      b1 <- .B1(n, i)
+      if (b1 == 0) 0 else {
+        vapply(seq_len(n / 2), function (m) {
+          R(m, n) / b1 * sum(vapply(seq_len(min(i, m)), function (j) {
+            D(j, i, m, n) * sum(
+              .PRow(.P1, .B1, .P1, .B1, i, j, m, n, s),
+              .PRow(.P1, .B1, .P01, .B01, i, j, m, n, s),
+              .PRow(.P01, .B01, .P1, .B1, i, j, m, n, s)
+            )
+          }, double(1)))
+        }, double(1))
+      }
+    }
+  }
+  .P01 <- function (s, n, i) {
+    if (n == 2L && i == 1L) {
+      1
+    } else if (n == i) {
+      0
+    } else if (i == 0L) {
+      0
+    } else {
+      b01 <- .B01(n, i)
+      if (b01 == 0) 0 else {
+        vapply(seq_len(n / 2), function (m) {
+          R(m, n) / b01 * sum(vapply(seq_len(min(i, m)), function (j) {
+            D(j, i, m, n) * sum(
+              .PRow(.P1, .B1, .P1, .B1, i, j, m, n, s),
+              .PRow(.P1, .B1, .P01, .B01, i, j, m, n, s, s - 1),
+              .PRow(.P01, .B01, .P1, .B1, i, j, m, n, s, s - 1)
+            )
+          }, double(1)))
+        }, double(1))
+      }
+    }
+  }
+  .P0(0, 3, 2)
+  
+  # Return:
+  sum(
+    .P0(s, n, i) * .B0(n, i),
+    .P1(s, n, i) * .B1(n, i),
+    .P01(s, n, i) * .B01(n, i))
+}
+
+# TODO: Remove the below, or if necessary replace with results of Carter et al. 1990; Steel 1993
+
 #' Number of trees with one extra step
 #' @param \dots Vector or series of integers specifying the number of leaves
 #' bearing each distinct non-ambiguous token.
