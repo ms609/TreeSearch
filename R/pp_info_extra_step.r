7f21cd3ef7af853224c83e8520af4b021800e416
Martin Smith <martins@gmail.com>
2021-02-17 13:14:48 +0000

    Fix some of Maddison & Slatkin's typos

--- R/pp_info_extra_step.r
+++ R/pp_info_extra_step.r
@@ -211,16 +211,26 @@ MaddisonSlatkin <- function (a, b) {
   n <- sum(a, b)
   i <- min(a, b)
   
-  D <- function (j, i, m, n) {
+  # D: The probability that, in a randomly selected tree of _n_ taxa, _i_ of
+  # which have state 1, the smaller subclade of _m_ taxa will receive _j_ taxa
+  # with state 1
+  .D <- function (j, i, m, n) {
     choose(i, j) * choose(n - i, m - j) / choose(n, m)
   }
-  R <- function (m, n) {
-    prod(ifelse(n == m + m, 1L, 2L),
+  # R: The probability that in a randomly selected tree of _n_ leaves, the
+  # smaller of the two basal subclades will have _m_ leaves.
+  # My function gives half the value given in Maddison & Slatkin 1991 [!]
+  .R <- function (m, n) {
+    prod(ifelse(n == m + m, 1L, 2L) / 2L, 
          choose(n, m),
          NRooted(m),
          NRooted(n - m)
     ) / NRooted(n)
-  }
+  } #TODO UNTESTED: Is it quicker to use LogNRooted?
+  
+  # B{b}: The probability that state _b_ is reconstructed at the base of a clade
+  # with _n_ leaves and _i_ leaves with state 1.
+  # My function runs from j = 0 to i, not j = 1 to i as printed in M&S91 [!]
   .B0 <- function (n, i) {
     if (n == 2L && i == 1L) {
       0
@@ -230,8 +240,8 @@ MaddisonSlatkin <- function (a, b) {
       1
     } else {
       sum(vapply(seq_len(n / 2L), function (m) {
-        R(m, n) * sum(vapply(seq_len(min(i, m)), function (j) {
-          D(j, i, m, n) * sum(
+        .R(m, n) * sum(vapply(0:(min(i, m)), function (j) {
+          .D(j, i, m, n) * sum(
             .B0(m, j) * .B0(n - m, i - j),
             .B01(m, j) * .B0(n - m, i - j),
             .B0(m, j) * .B01(n - m, i - j)
@@ -249,8 +259,8 @@ MaddisonSlatkin <- function (a, b) {
       0
     } else {
       sum(vapply(seq_len(n / 2L), function (m) {
-        R(m, n) * sum(vapply(seq_len(min(i, m)), function (j) {
-          D(j, i, m, n) * sum(
+        .R(m, n) * sum(vapply(0:min(i, m), function (j) {
+          .D(j, i, m, n) * sum(
             .B1(m, j) * .B1(n - m, i - j),
             .B01(m, j) * .B1(n - m, i - j),
             .B1(m, j) * .B01(n - m, i - j)
@@ -268,8 +278,8 @@ MaddisonSlatkin <- function (a, b) {
       0
     } else {
       sum(vapply(seq_len(n / 2L), function (m) {
-        R(m, n) * sum(vapply(seq_len(min(i, m)), function (j) {
-          D(j, i, m, n) * sum(
+        .R(m, n) * sum(vapply(0:min(i, m), function (j) {
+          .D(j, i, m, n) * sum(
             .B01(m, j) * .B01(n - m, i - j),
             .B0(m, j) * .B1(n - m, i - j),
             .B1(m, j) * .B0(n - m, i - j)
@@ -285,6 +295,10 @@ MaddisonSlatkin <- function (a, b) {
       P2(s - r, n - m, i - j),
       B2(n - m, i - j)), double(1)))
   }
+  
+  # P: The probability of having _s_ steps in a clade of _n_ leaves given that
+  # _i_ of the leaves have state 1.
+  # P{b}: P, given state _b_ is reconstructed at the base of the clade.
   .P0 <- function (s, n, i) {
     if (n == 2L && i == 1L) {
       if (s == 1L) 1 else 0
@@ -295,15 +309,15 @@ MaddisonSlatkin <- function (a, b) {
     } else {
       b0 <- .B0(n, i)
       if (b0 == 0) 0 else {
-        vapply(seq_len(n / 2), function (m) {
-          R(m, n) / b0 * sum(vapply(seq_len(min(i, m)), function (j) {
-            D(j, i, m, n) * sum(
+        sum(vapply(seq_len(n / 2), function (m) {
+          .R(m, n) / b0 * sum(vapply(seq_len(min(i, m)), function (j) {
+            .D(j, i, m, n) * sum(
               .PRow(.P0, .B0, .P0, .B0, i, j, m, n, s),
               .PRow(.P0, .B0, .P01, .B01, i, j, m, n, s),
               .PRow(.P01, .B01, .P0, .B0, i, j, m, n, s)
               )
           }, double(1)))
-        }, double(1))
+        }, double(1)))
       }
     }
   }
@@ -317,15 +331,15 @@ MaddisonSlatkin <- function (a, b) {
     } else {
       b1 <- .B1(n, i)
       if (b1 == 0) 0 else {
-        vapply(seq_len(n / 2), function (m) {
-          R(m, n) / b1 * sum(vapply(seq_len(min(i, m)), function (j) {
-            D(j, i, m, n) * sum(
+        sum(vapply(seq_len(n / 2), function (m) {
+          .R(m, n) / b1 * sum(vapply(seq_len(min(i, m)), function (j) {
+            .D(j, i, m, n) * sum(
               .PRow(.P1, .B1, .P1, .B1, i, j, m, n, s),
               .PRow(.P1, .B1, .P01, .B01, i, j, m, n, s),
               .PRow(.P01, .B01, .P1, .B1, i, j, m, n, s)
             )
           }, double(1)))
-        }, double(1))
+        }, double(1)))
       }
     }
   }
@@ -339,19 +353,32 @@ MaddisonSlatkin <- function (a, b) {
     } else {
       b01 <- .B01(n, i)
       if (b01 == 0) 0 else {
-        vapply(seq_len(n / 2), function (m) {
-          R(m, n) / b01 * sum(vapply(seq_len(min(i, m)), function (j) {
-            D(j, i, m, n) * sum(
+        sum(vapply(seq_len(n / 2), function (m) {
+          .R(m, n) / b01 * sum(vapply(seq_len(min(i, m)), function (j) {
+            .D(j, i, m, n) * sum(
               .PRow(.P1, .B1, .P1, .B1, i, j, m, n, s),
               .PRow(.P1, .B1, .P01, .B01, i, j, m, n, s, s - 1),
               .PRow(.P01, .B01, .P1, .B1, i, j, m, n, s, s - 1)
             )
           }, double(1)))
-        }, double(1))
+        }, double(1)))
       }
     }
   }
   .P0(0, 3, 2)
+  .P1(0, 3, 2)
+  .P01(0, 3, 2)
+  
+  .P0(0, 3, 1)
+  .P1(0, 3, 1)
+  .P01(0, 3, 1)
+  
+  .P0(1, 3, 1)
+  .P1(1, 3, 1)
+  .P01(1, 3, 1)
+  
+  P1(1, 3, 2)
+  .P01(1, 3, 2)
   
   # Return:
   sum(
