9e0c7046706e340283bf7000a906dfef13d62031
Martin Smith <martins@gmail.com>
2021-02-17 09:52:03 +0000

    Decisiveness() beginnings

---
+++ R/decisiveness.R
@@ -0,0 +1,30 @@
+#' Decisivness
+#' 
+#' Calculate the "Information Decisiveness" statistic of Goloboff (1991)
+#' 
+#' "Information decisiveness" is the extent to which a dataset supports "a
+#' choice or decision between different classifications" (Goloboff 1991).
+#' 
+#' A dataset with a high decisiveness provives a good basis for preferring
+#' certain trees over others.
+#' 
+#TODO add to pkgdown index
+#' @param 
+#' 
+#' @return `Decisiveness()` returns the value of Goloboff's (1991) decisiveness
+#' statistic, a measure of the extent to which a dataset provides strong support
+#' to its associated most parsimonious trees.
+#' 
+#' @references 
+#' \insertRef{Goloboff1991}{TreeSearch}
+#' @export
+Decisiveness <- function (dataset, bestScore) {
+  # sBar = mean number of steps for a character on a tree
+  
+  s <- bestScore
+  m <- sum(MinimumSteps(dataset))
+  
+  # Return:
+  (sBar - s) / (sBar - m)
+  
+}
