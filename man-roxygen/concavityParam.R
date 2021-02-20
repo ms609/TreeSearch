1e7c2c5f32199de212e97c9c25ad40866d50017e
Martin Smith <martins@gmail.com>
2021-02-20 09:04:56 +0000

    Update concavityParam.R

--- man-roxygen/concavityParam.R
+++ man-roxygen/concavityParam.R
@@ -1,3 +1,5 @@
 #' @param concavity A numeric value to use as the concavity constant (`k`)
 #'  in implied weighting. Defaults to 10; TNT sets a default of 3, but this 
-#'  is too low in some circumstances (Smith, 2019).
+#'  is too low in some circumstances (Smith, 2019). If `Inf`, applies equal
+#'  weights; if `'profile'`, then profile parsimony is used.
+#TODO rewrite
