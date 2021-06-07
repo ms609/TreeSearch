cf9314f05fc8b1e4337cf7033a2ed2f77e89dd0c
Martin Smith <martins@gmail.com>
2021-06-07 14:02:29 +0100

    Braz first passes

--- R/PlotCharacter.R
+++ R/PlotCharacter.R
@@ -85,14 +85,109 @@ PlotCharacter <- function (tree, dataset, char = 1L, ...) {
       if (all(inherited == aState)) {
         state[n, ] <- inherited
       } else if (any(lState & rState)) {
-        state[n, ] <- nState | (aState & (lState | rState))
+        #state[n, ] <- nState | (aState & (lState | rState))
+        state[n, ] <- aState & lState & rState
       } else {
         state[n, ] <- aState | nState
       }
     }
     
-  } else {
+    for (n in tips) {
+      nState <- state[n, ]
+      aState <- state[parentOf[n], ]
+      common <- aState & nState
+      if (any(common)) {
+        state[n, ] <- common
+      }
+    }
     
+  } else {
+    # Inapplicable Fitch, Brazeau, Guillerme & Smith 2019
+    inappLevel <- levels == '-'
+    appLevels <- !inappLevel
+    
+    # First downpass
+    for (n in postOrderNodes) {
+      lState <- state[left[n], ]
+      rState <- state[right[n], ]
+      common <- lState & rState
+      if (any(common)) { # 2
+        # If the token in common is only the inapplicable token, 
+        # and both descendants have an applicable token
+        if (all(common == inappLevel) && 
+            any(lState[appLevels]) && 
+            any(rState[appLevels])
+            ) {
+          # Set the node’s state to be the union of the descendants’ states
+          state[n, ] <- lState | rState
+        } else {
+          # set the node’s state to be the token in common between both descendants
+          state[n, ] <- common
+        }
+      } else { # 3
+        # If both descendants have an applicable token
+        if (any(lState[appLevels]) && any(rState[appLevels])) {
+          # set the node’s state to be the union of both descendants’ states
+          # without the inapplicable token
+          state[n, ] <- (lState | rState) & appLevels
+        } else {
+          # set the node’s state to be the union of its descendants’ states
+          state[n, ] <- lState | rState
+        }
+      }
+    }
+    
+    # First uppass
+    for (n in preOrderNodes) {
+      nState <- state[n, ]
+      aState <- state[parentOf[n], ]
+      lState <- state[left[n], ]
+      rState <- state[right[n], ]
+      # 1. If the node has the inapplicable token
+      if (any(nState[inappLevel])) {
+        # 2. If the node also has an applicable token
+        if (any(nState[appLevels])) {
+          # 3. If the node’s ancestor has the inapplicable token
+          if (any(aState[inappLevel])) {
+            # set the node’s state to be the inapplicable token only
+            state[n, ] <- inappLevel
+          } else {
+            # remove the inapplicable token from the current node’s state
+            state[n, ] <- nState & appLevels
+          }
+        } else {
+          # 4. If the node’s ancestor has the inapplicable token
+          if (any(aState[inappLevel])) {
+            # set the node’s state to be the inapplicable token only
+          } else {
+            # 5. If any of the descendants have an applicable token
+            if (any(lState[appLevels]) || any(rState[appLevels])) {
+              # set the node’s state to be the union of the applicable states
+              # of its descendants
+              state[n, ] <- (lState | rState) & appLevels
+            } else {
+              # set the node’s state to be the inapplicable token only
+              state[n, ] <- inappLevel
+            }
+          }
+        }
+      }
+    }
+    for (n in tips) {
+      nState <- state[n, ]
+      aState <- state[parentOf[n], ]
+      # 6. If the unvisited tip includes both inapplicable and applicable tokens
+      if (any(nState[inappLevel]) && any(nState[appLevels])) {
+        # 7. If the current node has only the inapplicable token
+        if (all(aState == inappLevel)) {
+          # set the tip’s state to the inapplicable token only
+          state[n, ] <- inappLevel
+        } else {
+          # remove the inapplicable token from the tip’s state
+          state[n, ] <- nState & appLevels
+        }
+      }
+    }
   }
     
   plot(tree)
