fe3acb258f1d8f7dac4a469629f26b604f8a167d
Martin Smith <martins@gmail.com>
2021-06-09 06:24:50 +0100

    Create test-PlotCharacter.R

---
+++ tests/testthat/test-PlotCharacter.R
@@ -0,0 +1,25 @@
+test_that("PlotCharacter()", {
+  
+  tree <- ape::read.tree(text = 
+   "((((((a, b), c), d), e), f), (g, (h, (i, (j, (k, l))))));")
+  Character <- function (str, plot = FALSE) {
+    PlotCharacter(tree, TreeTools::StringToPhyDat(str, tips = tree),
+                  edge.width = 2, plot = plot)
+  }
+  
+  expect_equal(structure(c(FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, 
+                           TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, 
+                           TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
+                           FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, 
+                           FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, 
+                           FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, 
+                           FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
+                           FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, 
+                           FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, 
+                           FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, 
+                           TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
+                           FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
+                           TRUE, FALSE, FALSE, FALSE, TRUE, TRUE), .Dim = c(23L, 5L), .Dimnames = list(
+                             NULL, c("-", "0", "1", "2", "3"))),
+               Character("23--1??--032"))
+})
