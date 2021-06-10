bc8a6bfbeab8cd90fa2184eb1a298bc7bfd36c19
Martin Smith <martins@gmail.com>
2021-06-10 17:08:16 +0100

    `TipInformation()` exploration

--- R/Rogues.R
+++ R/Rogues.R
@@ -53,3 +53,97 @@ TipInstability <- function (trees) {
            TipLabels(trees[[1]]))
   
 }
+
+#' Detect rogue taxa using splits contradicted
+#' @references 
+#' \insertRef{Aberer2013}{TreeSearch}
+#' \insertRef{Wilkinson2017}{TreeSearch}
+#' @examples 
+#' library("TreeTools", quietly = TRUE)
+#' trees <- AddTipEverywhere(BalancedTree(8), 'Rogue')
+#' trees[] <- lapply(trees, AddTip, 'Rogue', 'Rogue2')
+#' SplitBuster(trees)
+#' @importFrom TreeTools SplitInformation
+#' @importFrom TreeDist Entropy
+#' @export
+TipInformation <- function (trees) {
+  t1 <- trees[[1]]
+  nTip <- NTip(t1)
+  halfTip <- nTip / 2L
+  
+  rawSplits <- do.call(rbind,
+                       lapply(trees, function (tr) as.Splits(tr, t1)))
+  hash <- as.character(rawSplits)
+  dim(hash) <- dim(rawSplits)
+  hash <- apply(hash, 1, paste0, collapse = '')
+  uniqueSplits <- structure(class = "Splits", nTip = nTip,
+                            tip.label = TipLabels(t1),
+                            rawSplits[!duplicated(hash), , drop = FALSE])
+  weights <- table(hash)
+  # TODO improve efficiency by remaining in "raw" mode
+  splits <- as.logical(uniqueSplits)
+  splitH <- apply(splits, 1, SplitEntropy)['H1', ]
+  
+  nSplits <- length(splitH)
+  nMinus1 <- nSplits - 1L
+  
+  splitCombs <- combn(nSplits, 2)
+  combWeights <- weights[splitCombs]
+  dim(combWeights) <- dim(splitCombs)
+  combWeights <- apply(combWeights, 2, prod)
+  
+  miLosses <- apply(splitCombs, 2, function (i) {
+    split1 <- splits[i[1], ]
+    split2 <- splits[i[2], ]
+    
+    A1A2 <- sum(split1 & split2)
+    A1B2 <- sum(split1 & !split2)
+    B1A2 <- sum(!split1 & split2)
+    B1B2 <- sum(!split1 & !split2)
+    overlaps <- c(A1A2, A1B2, B1A2, B1B2)
+    
+    A1 <- A1A2 + A1B2
+    A2 <- A1A2 + B1A2
+    B1 <- B1A2 + B1B2
+    B2 <- A1B2 + B1B2
+    
+    jointHStart <- Entropy(overlaps / nSplits)
+    h1Start <- Entropy(c(A1, B1) / nSplits)
+    h2Start <- Entropy(c(A2, B2) / nSplits)
+    miStart <- h1Start + h2Start - jointHStart
+    
+    jointHAA <- Entropy((overlaps - c(1, 0, 0, 0)) / nMinus1)
+    jointHAB <- Entropy((overlaps - c(0, 1, 0, 0)) / nMinus1)
+    jointHBA <- Entropy((overlaps - c(0, 0, 1, 0)) / nMinus1)
+    jointHBB <- Entropy((overlaps - c(0, 0, 0, 1)) / nMinus1)
+    
+    h1AA <- Entropy(c(A1 - 1L, B1) / nMinus1)
+    h1AB <- Entropy(c(A1 - 1L, B1) / nMinus1)
+    h1BA <- Entropy(c(A1, B1 - 1L) / nMinus1)
+    h1BB <- Entropy(c(A1, B1 - 1L) / nMinus1)
+    
+    h2AA <- Entropy(c(A2 - 1L, B2) / nMinus1)
+    h2AB <- Entropy(c(A2 - 1L, B2) / nMinus1)
+    h2BA <- Entropy(c(A2, B2 - 1L) / nMinus1)
+    h2BB <- Entropy(c(A2, B2 - 1L) / nMinus1)
+    
+    miAA <- h1AA + h2AA - jointHAA
+    miAB <- h1AB + h2AB - jointHAB
+    miBA <- h1BA + h2BA - jointHBA
+    miBB <- h1BB + h2BB - jointHBB
+    
+    viAA <- jointHAA - miAA
+    viAB <- jointHAB - miAB
+    viBA <- jointHBA - miBA
+    viBB <- jointHBB - miBB
+    
+    miWithout <- ifelse(split1, ifelse(split2, miAA, miAB),
+                        ifelse(split2, miBA, miBB))
+    miLost <- miStart - miWithout
+    
+    # Return:
+    miLost
+  })
+  colSums(t(miLosses) * combWeights)
+  
+}
--- tests/testthat/test-Rogues.R
+++ tests/testthat/test-Rogues.R
@@ -4,4 +4,13 @@ test_that("Rogues found with TipInstability()", {
   trees <- AddTipEverywhere(BalancedTree(8), 'Rogue')
   instab <- TipInstability(trees)
   expect_equal('Rogue', names(which.max(instab)))
+  mi <- TipInformation(trees)
+  expect_equal('Rogue', names(mi[mi > 0]))
+  
+})
+
+test_that("Wilkinson & Crotti's examples are satisfied", {
+  fig2 <- list(AddTip(BalancedTree(6:1), '3', 'X'),
+               AddTip(BalancedTree(6:1), '4', 'X'))
+  
 })
