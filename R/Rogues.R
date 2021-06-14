324f19c8b993f02db0f8d82e1d3c32ab91dbf310
Martin Smith <martins@gmail.com>
2021-06-14 07:31:44 +0100

    BestConsensus()

--- R/Rogues.R
+++ R/Rogues.R
@@ -54,7 +54,7 @@ TipInstability <- function (trees) {
   
 }
 
-#' Detect rogue taxa using splits contradicted
+#' Detect rogue taxa using phylogenetic information distance
 #' @references 
 #' \insertRef{Aberer2013}{TreeSearch}
 #' \insertRef{Wilkinson2017}{TreeSearch}
@@ -64,222 +64,10 @@ TipInstability <- function (trees) {
 #' trees[] <- lapply(trees, AddTip, 'Rogue', 'Rogue2')
 #' 
 #' TipInformation(trees)
-#' @importFrom TreeDist SplitEntropy Entropy
-#' @export
-TipInformation <- function (trees) {
-  t1 <- trees[[1]]
-  nTip <- NTip(t1)
-  nMinus1 <- nTip - 1L
-  
-  rawSplits <- do.call(rbind,
-                       lapply(trees, function (tr) as.Splits(tr, t1)))
-  hash <- as.character(rawSplits)
-  dim(hash) <- dim(rawSplits)
-  hash <- apply(hash, 1, paste0, collapse = '')
-  uniqueSplits <- structure(class = "Splits", nTip = nTip,
-                            tip.label = TipLabels(t1),
-                            rawSplits[!duplicated(hash), , drop = FALSE])
-  weights <- table(hash, dnn = NULL)[unique(hash)]
-  # TODO improve efficiency by remaining in "raw" mode
-  splits <- as.logical(uniqueSplits)
-  splitH <- apply(splits, 1, SplitEntropy)['H1', ]
-  
-  nSplits <- length(splitH)
-  
-  splitCombs <- combn(nSplits, 2)
-  combWeights <- weights[splitCombs]
-  dim(combWeights) <- dim(splitCombs)
-  combWeights <- apply(combWeights, 2, prod)
-  
-  fromDiff <- apply(splitCombs, 2, function (i) {
-    split1 <- splits[i[1], ]
-    split2 <- splits[i[2], ]
-    rbind(split1, split2)
-    ifelse(split1, ifelse(split2, "TT", "TF"), ifelse(split2, "FT", "FF"))
-    
-    T1T2 <- sum(split1 & split2)
-    T1F2 <- sum(split1 & !split2)
-    F1T2 <- sum(!split1 & split2)
-    F1F2 <- sum(!split1 & !split2)
-    overlaps <- c(T1T2, T1F2, F1T2, F1F2)
-    names(overlaps) <- c('TT', 'TF', 'FT', 'FF') #TODO delete
-    
-    T1 <- T1T2 + T1F2
-    T2 <- T1T2 + F1T2
-    F1 <- F1T2 + F1F2
-    F2 <- T1F2 + F1F2
-    
-    jointHStart <- Entropy(overlaps / nTip)
-    h1Start <- Entropy(c(T1, F1) / nTip)
-    h2Start <- Entropy(c(T2, F2) / nTip)
-    miStart <- h1Start + h2Start - jointHStart
-    viStart <- jointHStart - miStart
-    
-    
-    jointHTT <- Entropy((overlaps - c(1, 0, 0, 0)) / nMinus1)
-    jointHTF <- Entropy((overlaps - c(0, 1, 0, 0)) / nMinus1)
-    jointHFT <- Entropy((overlaps - c(0, 0, 1, 0)) / nMinus1)
-    jointHFF <- Entropy((overlaps - c(0, 0, 0, 1)) / nMinus1)
-    ifelse(split1, ifelse(split2, jointHTT, jointHTF),
-           ifelse(split2, jointHFT, jointHFF))
-    
-    h1TT <- Entropy(c(T1 - 1L, F1) / nMinus1)
-    h1TF <- h1TT
-    h1FT <- Entropy(c(T1, F1 - 1L) / nMinus1)
-    h1FF <- h1FT
-    h2TT <- Entropy(c(T2 - 1L, F2) / nMinus1)
-    h2FT <- h2TT
-    h2TF <- Entropy(c(T2, F2 - 1L) / nMinus1)
-    h2FF <- h2TF
-    rbind(h1 = ifelse(split1, ifelse(split2, h1TT, h1TF), ifelse(split2, h1FT, h1FF)),
-          h2 = ifelse(split1, ifelse(split2, h2TT, h2TF), ifelse(split2, h2FT, h2FF)))
-    
-    
-    
-    miTT <- h1TT + h2TT - jointHTT
-    miTF <- h1TF + h2TF - jointHTF
-    miFT <- h1FT + h2FT - jointHFT
-    miFF <- h1FF + h2FF - jointHFF
-    
-    viTT <- jointHTT - miTT
-    viTF <- jointHTF - miTF
-    viFT <- jointHFT - miFT
-    viFF <- jointHFF - miFF
-    
-    miWithout <- ifelse(split1, ifelse(split2, miTT, miTF),
-                        ifelse(split2, miFT, miFF))
-    viWithout <- ifelse(split1, ifelse(split2, viTT, viTF),
-                        ifelse(split2, viFT, viFF))
-    miGained <- miWithout - miStart
-    viLost <- viStart - viWithout
-    
-    # Return:
-    miGained + viLost
-  })
-  
-  sameSplits <- weights > 1L
-  fromSame <- apply(splits[sameSplits, , drop = FALSE], 1, function (split) {
-    m <- sum(split)
-    n <- nTip - m
-    h0 <- Entropy(c(m, n) / nTip)
-    hT <- Entropy(c(m - 1L, n) / nMinus1)
-    hF <- Entropy(c(m, n - 1L) / nMinus1)
-    
-    hWithout <- ifelse(split, hT, hF)
-    miGained <- h0 - hWithout
-    
-    # Return:
-    miGained + 0
-  })
-  
-  colSums(t(fromSame) * as.integer(weights[sameSplits] - 1L)) +
-    colSums(t(fromDiff) * combWeights)
-  
-  # TODO This overwrites... 
-  siGain <- apply(splitCombs, 2, function (i) {
-    split1 <- splits[i[1], ]
-    split2 <- splits[i[2], ]
-    ifelse(split1, ifelse(split2, "TT", "TF"), ifelse(split2, "FT", "FF"))
-    
-    T1T2 <- sum(split1 & split2)
-    T1F2 <- sum(split1 & !split2)
-    F1T2 <- sum(!split1 & split2)
-    F1F2 <- sum(!split1 & !split2)
-    overlaps <- c(T1T2, T1F2, F1T2, F1F2)
-    names(overlaps) <- c('TT', 'TF', 'FT', 'FF') #TODO delete
-    T1 <- T1T2 + T1F2
-    F1 <- F1T2 + F1F2
-    T2 <- T1T2 + F1T2
-    F2 <- T1F2 + F1F2
-    i0 <- SplitInformation(T1, F1) + SplitInformation(T2, F2)
-    noOverlap <- !as.logical(overlaps)
-    
-    iWithout <- 
-    if (any(noOverlap)) {
-      stopifnot(sum(noOverlap) == 1L) # as splits are non-identical
-      overT1 <- c(F2, T2, T2, F2)[noOverlap]
-      si0 <- SplitSharedInformation(nTip, T1, overT1)
-      
-      overT1TT <- c(F2, T2 - 1L, T2 - 1L, F2)[noOverlap]
-      overT1TF <- c(F2 - 1L, T2, T2, F2 - 1L)[noOverlap]
-      overT1FT <- c(F2, T2 - 1L, T2 - 1L, F2)[noOverlap]
-      overT1FF <- c(F2 - 1L, T2, T2, F2 - 1L)[noOverlap]
-      
-      siTT <- SplitSharedInformation(nMinus1, T1 - 1L, overT1TT)
-      siTF <- SplitSharedInformation(nMinus1, T1 - 1L, overT1TF)
-      siFT <- SplitSharedInformation(nMinus1, T1, overT1FT)
-      siFF <- SplitSharedInformation(nMinus1, T1, overT1FF)
-      
-      siWithout <- ifelse(split1, ifelse(split2, siTT, siTF),
-                          ifelse(split2, siFT, siFF))
-    } else {
-      si0 <- 0
-      si <- double(4)
-      for (j in which(overlaps == 1)) {
-        si[j] <- switch(j,
-                        SplitSharedInformation(nMinus1, T1 - 1L, F2),
-                        SplitSharedInformation(nMinus1, T1 - 1L, T2),
-                        SplitSharedInformation(nMinus1, T1, T2 - 1L),
-                        SplitSharedInformation(nMinus1, T1, F2 - 1L))
-      }
-      si
-      siWithout <- si[ifelse(split1, ifelse(split2, 1, 2),
-                             ifelse(split2, 3, 4))]
-      
-    }
-    siGain <- siWithout - si0
-    di0 <- i0 - si0 - si0
-    di <- c(SplitInformation(T1 - 1L, F1) + SplitInformation(T2 - 1L, F2),
-            SplitInformation(T1 - 1L, F1) + SplitInformation(T2, F2 - 1L),
-            SplitInformation(T1, F1 - 1L) + SplitInformation(T2 - 1L, F2),
-            SplitInformation(T1, F1 - 1L) + SplitInformation(T2, F2 - 1L))
-    diWithout <- di[ifelse(split1, ifelse(split2, 1, 2),
-                           ifelse(split2, 3, 4))]
-    diGain <- diWithout - di0
-    
-    # Return:
-    siGain - diGain
-  })
-  
-  sameSplits <- weights > 1L
-  fromSame <- apply(splits[sameSplits, , drop = FALSE], 1, function (split) {
-    m <- sum(split)
-    n <- nTip - m
-    si0 <- SplitInformation(m, n)
-    siT <- SplitInformation(m - 1L, n)
-    siF <- SplitInformation(m, n - 1L)
-    
-    siWithout <- ifelse(split, siT, siF)
-    i0 <- si0
-    iWithout <- siWithout
-    di0 <- 0
-    diWithout <- 0
-    
-    siGain <- siWithout - si0
-    diGain <- diWithout - di0
-    
-    # Return:
-    siGain - diGain
-  })
-  colSums(t(fromSame) * as.integer(weights[sameSplits] - 1L)) +
-  colSums(t(siGain) * combWeights)
-  
-}
-
-#' @importFrom TreeDist ClusteringInfoDistance ClusteringInfo
-#' @export
-CITipRemover <- function (trees) {
-  tips <- trees[[1]]$tip.label
-  startInfo <- mean(ClusteringInfo(trees))
-  info <- vapply(tips, function (drop) {
-    tr <- lapply(trees, drop.tip, drop)
-    c(meanInfo = mean(ClusteringInfo(tr)),
-      meanDist = mean(ClusteringInfoDistance(tr)))
-  }, double(2))
-  (mean(ClusteringInfoDistance(trees)) - info['meanDist', ]) /
-  (startInfo / info['meanInfo', ])
-}
-
+#' sb <- TipInformation(trees)
+#' col <- hcl.colors(ceiling(max(sb) *1.13), 'inferno')[ceiling(sb)]
+#' plot(consensus(trees), tip.color = col)
+#' plot(ConsensusWithout(trees, names(sb[sb == max(sb)])))
 #' @importFrom TreeDist PhylogeneticInfoDistance CladisticInfo
 #' @export
 TipRemover <- function (trees) {
@@ -293,3 +81,42 @@ TipRemover <- function (trees) {
   (mean(PhylogeneticInfoDistance(trees)) - info['meanDist', ]) /
   (startInfo / info['meanInfo', ])
 }
+
+#' Calculate the most informative consensus tree
+#' 
+#' Uses the splitwise information content as a shortcut, which involves double
+#' counting of some information (which may or may not be desirable) but is
+#' calculable in polynomial ratehr than exponential time.
+#' 
+#' @template MRS
+#' @importFrom ape consensus
+#' @importFrom TreeDist SplitwiseInfo SplitFrequency
+#' @export
+BestConsensus <- function (trees) {
+  if (!inherits(trees, 'multiPhylo')) {
+    if (inherits(trees, 'phylo')) {
+      return (trees)
+    }
+    if (!is.list(trees)) {
+      stop("`trees` must be a list of `phylo` objects")
+    }
+    trees <- structure(trees, class = 'multiPhylo')
+  }
+  trees <- lapply(trees, RenumberTips, trees[[1]])
+  trees <- lapply(trees, Preorder)
+  nTree <- length(trees)
+  
+  cons <- vector('list', nTip - 2L)
+  info <- double(nTip - 2)
+  cons[[1]] <- consensus(trees, p = 0.50)
+  info[1] <- SplitwiseInfo(cons[[1]], SplitFrequency(cons[[1]], trees) / nTree)
+  for (i in 1 + seq_len(nTip - 3L)) {
+    tipScores <- TipRemover(trees)
+    candidate <- which.max(tipScores)
+    trees <- lapply(trees, drop.tip, candidate)
+    cons[[i]] <- tr <- consensus(trees, p  = 0.50)
+    info[i] <- SplitwiseInfo(tr, SplitFrequency(tr, trees) / nTree)
+  }
+  cons[[which.max(info)]]
+}
+  
