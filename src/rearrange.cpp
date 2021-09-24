6a4485e2956be1b0c68f4c56bc53559990d85fb1
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-24 09:48:58 +0100

    const ints

--- src/rearrange.cpp
+++ src/rearrange.cpp
@@ -5,6 +5,14 @@ using namespace Rcpp;
 
 //  [[Rcpp::export]]
 List asan_error (const IntegerMatrix x) {
+  const int
+    n_edge = x.nrow(),
+    n_internal = n_edge / 2,
+    n_tip = n_internal + 1,
+    n_vert = n_internal + n_tip,
+    root_node = n_tip + 1
+  ;
+  
   Rf_error("Oh dear.");
   return List::create();
 }
