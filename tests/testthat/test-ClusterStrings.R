f7d8c5ffb63168ec7e84e982b83292d89bcf3998
Martin R. Smith <pjjg18@durham.ac.uk>
2022-03-23 09:02:36 +0000

    Leak @ 5

--- tests/testthat/test-ClusterStrings.R
+++ tests/testthat/test-ClusterStrings.R
@@ -1,8 +1,8 @@
 test_that("ClusterStrings() works", {
-  skip("Locate memory leak, #TODO remove")
   x <- rep(letters[1:6], 1:6)
   expect_equal(ClusterStrings(x),
                structure(rep(1:6, 1:6), 'med' = letters[1:6]))
+  skip("Locate memory leak @5, #TODO remove")
   expect_error(ClusterStrings(x, 1), "`maxCluster` must be at least two.")
   expect_equal(range(ClusterStrings(x, 2)), 1:2)
   expect_equal(ClusterStrings(paste0(c('aaaa', 'bbb', 'cccccc'), 1:20)),
