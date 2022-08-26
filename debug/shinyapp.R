8c08485af6b6311ac840c91bfccce028ee151fe0
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-08-26 09:29:02 +0100

    Update shinyapp.R

--- debug/shinyapp.R
+++ debug/shinyapp.R
@@ -1,5 +1,6 @@
+
 input <- list(
-  dataSource = "Agnarsson2004",
+  dataSource = "Sun2018",
   mapDisplay = "",
   plotFormat = "ind"
 )
@@ -14,6 +15,24 @@ r <- list(
 )
 
 r$plottedTree = r$trees[[1]]
+concavity <- function() 10
+n=3
+
+rogueCont <- PolEscapa(r$trees,
+                       r$dataset[r$trees[[1]][["tip.label"]], n],
+                       concavity())
+roguishness <- if (max(rogueCont) == 0) {
+  "black"
+} else {
+  hcl.colors(256, "inferno")[
+    (192 * rogueCont / max(rogueCont)) + 1
+  ]
+}
+par(mar = rep(0, 4), cex=0.9)
+PlotCharacter(r$plottedTree, r$dataset, n,
+              edge.width = 2.5,
+              updateTips = "updateTips" %in% input$mapDisplay,
+              tip.color = roguishness)
 
 distances <- ClusteringInfoDist(r$trees)
 mapped <- cmdscale(distances, k = 6)
