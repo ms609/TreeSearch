36f2423d6441ba8e4d3115cc3860a49483ab5e6d
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-08-26 11:40:57 +0100

    Test with no inapplicables

--- tests/testthat/test-PolEscapa.R
+++ tests/testthat/test-PolEscapa.R
@@ -30,6 +30,15 @@ test_that("LengthAdded()", {
   # Single tree
   expect_equal(LengthAdded(trees[[1]], dataset[, 10]), pe10)
   
+  # No inapplicables
+  appData <- dataset
+  colnames(attr(appData, "contrast"))[1] <- "x"
+  attr(appData, "levels")[1] <- "x"
+  attr(appData, "allLevels")[4] <- "x"
+  pe10 <- LengthAdded(trees, appData[, 10])
+  expect_equal(pe10["Neopilina"], c(Neopilina = 1))
+  expect_equal(sum(pe10), 1)
+  
   # Implied weighting
   expect_equal(
     unname(PolEscapa(trees, dataset[, 11], concavity = 5)["Neopilina"]),
