1dd3d212e5ca788dc41e8fd482647ec17ef78a63
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-09-02 15:55:09 +0100

    Use %1 for vars

--- split-support/tnt.R
+++ split-support/tnt.R
@@ -4,16 +4,16 @@ if(!dir.exists("split-support/alignments")) {
   source("split-support/simulate.R")
 }
 
-template <- readLines("split-support/tnt-template.run")
-on.exit(unlink("split-support/tnt.run"))
-
 for (aln in alns) {
-  if (file.exists(paste0("split-support/tnt/", aln, ".iw16.sym"))) {
+  if (file.exists(paste0("split-support/tnt/", aln, ".ew.log"))) {
     message("Results found for ", aln)
   } else {
-    writeLines(gsub("aln####", aln, template, fixed = TRUE),
-              "split-support/tnt.run")
-    system2(tntExec, "proc split-support/tnt.run")
+    system2(
+      tntExec, 
+      paste0("proc split-support/tnt-ew.run ",
+             "split-support/alignments/", aln, ".nex ",
+             "split-support/TNT/", aln, ".ew.log")
+    )
   }
 }
 
