2d534122048ad8c89ba8c1e3245d124d52f74faf
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-08-26 11:16:33 +0100

    dataset via ReadCharacters()

--- man-roxygen/datasetParam.r
+++ man-roxygen/datasetParam.r
@@ -1,3 +1,4 @@
 #' @param dataset A phylogenetic data matrix of \pkg{phangorn} class
 #' \code{phyDat}, whose names correspond to the labels of any accompanying tree.
+#' Perhaps load into R using \code{\link[TreeTools]{ReadCharacters}}.
 # Defined in TreeTools.  Please propagate any changes there.
