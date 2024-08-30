64c76c621ee26da64182c5259b6ae4812da65f65
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-08-30 09:17:59 +0100

    RNGversion

--- tests/testthat/test-Jackknife.R
+++ tests/testthat/test-Jackknife.R
@@ -11,7 +11,7 @@ test_that("Jackknife supports are correct", {
   expect_error(Jackknife(start_tree, dataset, resampleFreq = 0))
   expect_error(Jackknife(start_tree, dataset, resampleFreq = 9/10))
   
-  RNGversion("3.6.0")
+  suppressWarnings(RNGversion("3.5.0"))
   set.seed(0)
   
   strict <- TreeSearch(start_tree, dataset, verbosity = 0)
