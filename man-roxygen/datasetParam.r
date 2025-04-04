5750f2515ef2855c30555d5c72d4c7be520f7a82
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-04-04 15:46:23 +0100

    Read as PhyDat

--- man-roxygen/datasetParam.r
+++ man-roxygen/datasetParam.r
@@ -1,4 +1,4 @@
 #' @param dataset A phylogenetic data matrix of \pkg{phangorn} class
 #' \code{phyDat}, whose names correspond to the labels of any accompanying tree.
-#' Perhaps load into R using \code{\link[TreeTools]{ReadCharacters}}.
+#' Perhaps load into R using \code{\link[TreeTools]{ReadAsPhyDat}}.
 # Defined in TreeTools.  Please propagate any changes there.
