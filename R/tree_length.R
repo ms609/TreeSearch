593fd665ea30755224c43cbbcf887fcd5eeebe5e
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-04-09 16:21:21 +0100

    parentOfParam

--- R/tree_length.R
+++ R/tree_length.R
@@ -373,7 +373,9 @@ MorphyLength <- function (parent, child, morphyObj, inPostorder = FALSE,
 }
 
 #' @describeIn MorphyTreeLength Fastest function that requires internal tree parameters
-#' @template parentOfParam
+#' @param parentOf Integer vector containing, for each tip and each node in 
+#' sequential order, the integer index its parent node.  
+#' The root node should be its own parent.
 #' @param leftChild integer vector containing, for each node, starting at the 
 #' root and proceeding in sequential order, the integer corresponding to its
 #' left child.  Tip numbering begins at 0; the root node is numbered `nTip`.
