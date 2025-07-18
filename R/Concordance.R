8004459eee5c609d72fc7b46818d1c18e012d981
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-07-18 14:39:26 +0100

    exports

--- R/Concordance.R
+++ R/Concordance.R
@@ -450,6 +450,7 @@ QCol <- function(amount, quality) {
 #' @param n Integer vector giving number of cells to plot in swatch for 
 #' `quality` and `amount`.
 #' @inheritParams ConcordanceTable
+#' @export
 QALegend <- function(where = c(0.1, 0.3, 0.1, 0.3), n = 5, Col = QACol) {
   oPar <- par(fig = where, new = TRUE, mar = rep(0, 4), xpd = NA)
   on.exit(par(oPar))
