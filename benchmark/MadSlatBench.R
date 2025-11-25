6deacb80f83eae3a9520754207e6898f571358bb
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-11-25 11:52:26 +0000

    Benchmarking (temporary?)

---
+++ benchmark/MadSlatBench.R
@@ -0,0 +1,18 @@
+library("TreeSearch")
+states <- c(2, 3, 0, 4, 0, 0, 0, 2)
+
+bench::mark(
+  setup = MaddisonSlatkin_clear_cache(),
+  control = 1 + 1,
+  single = MaddisonSlatkin(3L, states),
+  multi  = MaddisonSlatkin_steps(0L, 6L, states),
+  max_iterations = 12,
+  check = FALSE
+)
+
+bench::mark(
+  single = {MaddisonSlatkin_clear_cache(); MaddisonSlatkin(3L, states)},
+  multi  = {MaddisonSlatkin_clear_cache(); MaddisonSlatkin_steps(0L, 4L, states)},
+  check = FALSE,
+  min_time = 20
+)
