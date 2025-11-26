d7159095ede958988d6c9f6ca47ce714b2655216
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-11-26 19:10:39 +0000

    return probability
    Divide by factorial(n) / factorial(groups)

--- tests/testthat/test-FixedTreeCount.R
+++ tests/testthat/test-FixedTreeCount.R
@@ -1,17 +1,22 @@
 test_that("All labellings counted", {
   
-  # https://stackoverflow.com/questions/22569176/how-to-generate-permutations-or-combinations-of-object-in-r
   labels <- c(2, 3, 2)
   nTip <- sum(labels)
   tree <- TreeTools::BalancedTree(nTip)
   allComb <- combinat::permn(rep(seq_along(labels), labels)) |>
+    unique() |>
     unlist() |>
     paste0(collapse = "") |>
     StringToPhyDat(tips = nTip, byTaxon = FALSE)
-  expect <- table(CharacterLength(tree, allComb, compress = TRUE))
-  
-  expect_equal(as.numeric(FixedTreeSteps(tr, 4, c(2, 3, 2))[3:5]),
-               as.numeric(expect))
-  
+  expect <- table(CharacterLength(tree, allComb, compress = TRUE)) / 
+    factorial(nTip) * prod(factorial(labels))
+  FixedTreeCount(tree, c(2, 3, 2), 5) / 24
+  expect_equal(FixedTreeCount(tree, c(7), 2),
+               c("0" = 1, "1" = 0, "2" = 0))
+  expect_equal(FixedTreeCount(tree, c(1, 6), 2),
+               c("0" = 0, "1" = 1, "2" = 0))
+               
+  expect_equal(FixedTreeCount(tree, c(2, 3, 2), 5),
+               c("0" = 0, "1" = 0, expect, "5" = 0))
   
 })
