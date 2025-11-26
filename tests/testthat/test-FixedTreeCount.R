8b5e6ffb2b2f69bf4c4878e64e334634fa6ab949
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-11-26 19:29:18 +0000

    Principled combn production

--- tests/testthat/test-FixedTreeCount.R
+++ tests/testthat/test-FixedTreeCount.R
@@ -3,13 +3,24 @@ test_that("All labellings counted", {
   labels <- c(2, 3, 2)
   nTip <- sum(labels)
   tree <- TreeTools::BalancedTree(nTip)
-  allComb <- combinat::permn(rep(seq_along(labels), labels)) |>
-    unique() |>
+  first <- labels[[1]]
+  leftAfterFirst <- nTip - first
+  second <- labels[[2]]
+  nComb <- choose(nTip, first) * choose(leftAfterFirst, second)
+  allComb <- apply(combn(nTip, first), 2, function(i) {
+    apply(combn(leftAfterFirst, second), 2, function(j) {
+      ret <- rep(1, nTip)
+      ret2 <- rep(3, leftAfterFirst)
+      ret2[j] <- 2
+      ret[-i] <- ret2
+      ret
+    }, simplify = FALSE)
+  }) |>
     unlist() |>
     paste0(collapse = "") |>
     StringToPhyDat(tips = nTip, byTaxon = FALSE)
-  expect <- table(CharacterLength(tree, allComb, compress = TRUE)) / 
-    factorial(nTip) * prod(factorial(labels))
+  expect <- table(CharacterLength(tree, allComb, compress = TRUE)) / nComb
+    
   FixedTreeCount(tree, c(2, 3, 2), 5) / 24
   expect_equal(FixedTreeCount(tree, c(7), 2),
                c("0" = 1, "1" = 0, "2" = 0))
