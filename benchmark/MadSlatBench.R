02d94729d74c32200fe300569cbdc0e7d69a6f38
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-11-26 06:32:08 +0000

    rm _steps

--- benchmark/MadSlatBench.R
+++ benchmark/MadSlatBench.R
@@ -3,14 +3,14 @@ states <- c(2, 3, 0, 4, 0, 0, 0, 2)
 
 bench::mark(
   single = {MaddisonSlatkin_clear_cache(); MaddisonSlatkin(3L, states)},
-  multi  = {MaddisonSlatkin_clear_cache(); MaddisonSlatkin_steps(0L, 4L, states)},
+  multi  = {MaddisonSlatkin_clear_cache(); MaddisonSlatkin(0:4, states)},
   max_iterations = 12,
   check = FALSE
 )
 
 bench::mark(
   single = {MaddisonSlatkin_clear_cache(); MaddisonSlatkin(3L, states)},
-  multi  = {MaddisonSlatkin_clear_cache(); MaddisonSlatkin_steps(0L, 4L, states)},
+  multi  = {MaddisonSlatkin_clear_cache(); MaddisonSlatkin(0:4, states)},
   check = FALSE,
   min_time = 20
 )
