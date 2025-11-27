c4b2c4d6274792e0bc0edebbdf4e7724c6ea4a36
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-11-27 13:30:47 +0000

    Update FTLBench.R

--- benchmark/FTLBench.R
+++ benchmark/FTLBench.R
@@ -1,4 +1,5 @@
 library("TreeTools")
+library("testthat")
 library("TreeSearch")
 
 tree <- ape::read.tree(
@@ -8,13 +9,20 @@ charDat <- StringToPhyDat(char, TipLabels(tree))
 if (interactive()) {
   PlotCharacter(tree, charDat)
 }
+m <- 3
+expect_equal(MinimumLength(char, tree), m)
+s <- 5
+expect_equal(TreeLength(tree, charDat), s)
+h <- s - m
+g <- 7
+r <- (g - s) / (g - m)
 nullLengths <- c(mean = 5.8207, median = 6)
 expect_equal(
   Consistency(StringToPhyDat(char, TipLabels(tree)), tree, nRelabel = 100),
   c(ci = m / s, ri = r, rc = r * m / s,
     rhi = h / (nullLengths[["median"]] - m),
     rhiBar = h / (nullLengths[["mean"]] - m)),
-  tolerance = 0.01
+  tolerance = 0.05
 )
 
 expect_equal(
@@ -28,3 +36,5 @@ pd <- StringToPhyDat(char, TipLabels(tree))
 
 bench::mark(Consistency(pd, tree, nRelabel = 1000),
             Consistency(pd, tree, nRelabel = NULL), check = FALSE)
+
+bench::mark(Consistency(pd, tree, nRelabel = NULL), min_time = 32)
