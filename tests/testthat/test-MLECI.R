4ad14b5a99d6f3925fd7ba54368c7acc58a4588b
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-11-28 16:07:25 +0000

    test resampling

--- tests/testthat/test-MLECI.R
+++ tests/testthat/test-MLECI.R
@@ -5,10 +5,12 @@ test_that("MLCI works", {
                  c(0, 0, 0, 0, 0, 1, 1, 1),
                  c(0, 1, 0, 1, 0, 1, 0, 1))
   
-  expect_no_error(val <- MLCI(tree, chars, precision = 1e-2))
+  expect_no_error(val <- MLCI(tree, chars, precision = 1 / 20))
   val
   expect_equal(val[1, "mlci"], 1)
   expect_equal(val[2, "mlci"], 0)
   expect_gt(val[1, "mlci"], val[3, "mlci"])
   expect_lt(val[4, "mlci"], 0)
+  expect_lt(val[1, "nResample"], 10)
+  expect_gt(val[4, "nResample"], 5)
 })
